c493ced43d8362d5022d3cd6faef33a30b9afe45
Shadoweyes75 <62580251+Shadoweyes75@users.noreply.github.com>
2021-12-02 16:46:18 -0700

    added comments describing streamlit syntax

--- mock.py
+++ mock.py
@@ -1,22 +1,28 @@
 import streamlit as st
 from PIL import Image
 
+#page_config sets up the title of the page (this mainly affects the
+#text that appears on the browser tab as far as I know)
+#icon makes the icon on the browser tab into a chart emoji
 st.set_page_config(
         page_title = "ISTA 429 Vizualization Mockup",
         page_icon = "chart_with_upwards_trend",
         layout = "wide"
     )
-
+#st.markdown writes text onto the page
 st.markdown("# ISTA 429 Vizualization Mockup")
 
-
+#st.columns(n) creates n equally spaced columns on the page 
 col1, col2 = st.columns(2)
 
-
+#adds content to col1
 with col1:
+    #writes text as header of the graph
     st.header("Graphs")
     st.markdown("### RGB")
+    #opens an image file
     image = Image.open("pics/rgbGraphTemp.png")
+    #st.image displays an image on the screen
     st.image(image)
     
     st.markdown("### PS2")
@@ -29,6 +35,10 @@ with col1:
     
 with col2:
     st.header("PointCloud")
+    #st.selectbox(x, y) creates a dropdown menue
+    #x is the title for the dropdown box
+    #y is the different dropdown options
+    #stores selection into variable, in this case dateIn
     dateIn = st.selectbox(
         "Date",
         ("1-1-0000", "1-2-0000", "1-3-0000", "1-4-0000"))
