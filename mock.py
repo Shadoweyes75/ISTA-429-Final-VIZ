527dab72f97c9d17598a91456906fac398f9b313
Shadoweyes75 <62580251+Shadoweyes75@users.noreply.github.com>
2021-12-07 14:27:54 -0700

    Modified Dropdown Menues

--- mock.py
+++ mock.py
@@ -1,6 +1,17 @@
 import streamlit as st
 from PIL import Image
 
+#makes a dropdown box for all plants that match plot/geno/treat values
+#plot is the plot the plant is found in
+#geno is genotype of the plant wanted
+#treat is the treatmen of the plant
+def get_plants_for_dropdown(plot, geno, treat):
+        plantIn = st.selectbox(
+            "Plants",
+            ("plant1","plant2","plant3"))
+        return plantIn
+
+
 #page_config sets up the title of the page (this mainly affects the
 #text that appears on the browser tab as far as I know)
 #icon makes the icon on the browser tab into a chart emoji
@@ -35,19 +46,24 @@ with col1:
     
 with col2:
     st.header("PointCloud")
+    plant = st.text_input("Plant Name",
+                              "Plant 1")
     #st.selectbox(x, y) creates a dropdown menue
     #x is the title for the dropdown box
     #y is the different dropdown options
     #stores selection into variable, in this case dateIn
-    dateIn = st.selectbox(
-        "Date",
-        ("1-1-0000", "1-2-0000", "1-3-0000", "1-4-0000"))
+    plotIn = st.selectbox(
+        "Plot",
+        ("-","1", "2", "3", "4"))
+    treatmentIn = st.selectbox(
+        "Treatment",
+        ("-","1", "2", "3"))
     genoIn = st.selectbox(
         "Genotype",
-        ("gen1", "gen2", "gen3", "gen4"))
-    plantIn = st.selectbox(
-        "Individial Plant",
-        ("Plant 1", "Plant 2", "Plant 3", "Plant 4"))
+        ("-","gen1", "gen2", "gen3", "gen4"))
+    if(plotIn != "-" or treatmentIn != "-" or genoIn != "-"):
+        plant = get_plants_for_dropdown(plotIn, treatmentIn, genoIn)
+    
 
     image2 = Image.open("pics/lettuceTemp.jpg")
     st.image(image2)
